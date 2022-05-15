770f77614da7a3fdd55fc81de326469a9c8de260
Yuri <yurialves2@gmail.com>
2022-05-14 22:57:19 -0300

    chore: using system js

--- client/js/app-es6/services/NegociacaoService.js
+++ client/js/app-es6/services/NegociacaoService.js
@@ -1,4 +1,9 @@
-class NegociacaoService {
+import {HttpService} from './HttpService';
+import {ConnectionFactory} from './ConnectionFactory';
+import {NegociacaoDao} from '../dao/NegociacaoDao';
+import {Negociacao} from '../models/Negociacao';
+
+export class NegociacaoService {
     
     constructor() {
         
@@ -65,4 +70,57 @@ class NegociacaoService {
             throw new Error(erro);
         });
 	} 
+
+    cadastra(negociacao) {
+
+        return ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.adiciona(negociacao))
+            .then(() => 'Negociação adicionada com sucesso')
+            .catch(erro => {
+                console.log(erro);
+                throw new Error('Não foi possível adicionar a negociação')
+            });
+    }
+
+    lista() {
+
+        return ConnectionFactory
+                .getConnection()
+                .then(connection => new NegociacaoDao(connection))
+                .then(dao => dao.listaTodos())
+                .catch(erro => {
+                    console.log(erro);
+                    throw new Error('Não foi possível obter as negociações')
+                })
+    }
+
+    apaga() {
+
+        return ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.apagaTodos())
+            .then(() => 'Negociações apagadas com sucesso')
+            .catch(erro => {
+                console.log(erro);
+                throw new Error('Não foi possível apagar as negociações')
+            })
+
+    }
+
+    importa(listaAtual) {
+
+        return this.obterNegociacoes()
+            .then(negociacoes => 
+                negociacoes.filter(negociacao => 
+                    !listaAtual.some(negociacaoExistente => 
+                        JSON.stringify(negociacao) == JSON.stringify(negociacaoExistente)))
+            )
+            .catch(erro => {
+                console.log(erro);
+                throw new Error('Não foi possível buscar negociações para importar');
+            })
+    }
 }
